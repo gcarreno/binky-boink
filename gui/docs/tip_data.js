fb9a89d7eda7058ad51bc68dc2c4bedabf9e0bc9
Gustavo Carreno <guscarreno@gmail.com>
2011-02-20 18:02:56 +0000

    Added the gui folder to the documentation script.

---
+++ gui/docs/tip_data.js
@@ -0,0 +1,6 @@
+var s = new Array()
+
+s[0] = "binkyboink^binkyboink.html^^ ^0"
+s[1] = "frmMainUnit^frmMainUnit.html^^ ^0"
+s[2] = "frmMainUnit.TfrmMain^frmMainUnit.TfrmMain.html^^ ^0"
+s[3] = "frmMainUnit.frmMain^frmMainUnit.html#frmMain^^ ^0"
